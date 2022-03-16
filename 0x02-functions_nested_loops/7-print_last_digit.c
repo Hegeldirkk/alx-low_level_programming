c45ac9c6ef9464ea8857d588dea332059e04004e
Hegeldirkk <hegeldirkk@gmail.com>
2022-03-16 01:28:28 +0000

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,10 +8,12 @@
 
 int print_last_digit(int n)
 {
-	n = n % 10;
-
-	if (n < 0)
-		n = -n;
-		_putchar(n + '0');
-		return (n);
+n = n % 10;
+if (n < 0)
+{	
+   n = -n;
+   _putchar(n + '0');
+   return (n);
+}
+return (n);
 }
