6c472ac4e1ac4239e1a2280faf1144cbe1be6bec
Hegeldirkk <hegeldirkk@gmail.com>
2022-03-19 00:30:34 +0000

    nexted 4

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
-extern int _isupper(int c);
-extern int _isdigit(int c);
-extern int mul(int a, int b);
-extern void print_numbers(void);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
 #endif
