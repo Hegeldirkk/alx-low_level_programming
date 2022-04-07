eed64e9dfa80986740b922390c4bdccf27b80c83
Hegeldirkk <hegeldirkk@gmail.com>
2022-04-07 07:18:41 +0000

    tasks argumentAs

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,13 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
  * main - function
- * @argc: argv size array & number of command line arguments 
- * @argv: array of size argc & containing the program command line arguments
- * Return: 0.
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
  */
+
+
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
