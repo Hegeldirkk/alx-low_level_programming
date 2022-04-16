40fe5252bb090224892e4f17fd538889c66849d4
Hegeldirkk <hegeldirkk@gmail.com>
2022-04-16 13:59:35 +0000

    structures typedef task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef FILE_DOG
+#define FILE_DOG
+
+/**
+ * struct dog - Define a new type struct dog.
+ *
+ * @name: pointeur de variable name vers le noms de caractères dog.
+ * @age: variable de type float de dog.
+ * @owner: pointeur de variable owner vers le noms de caractères dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
