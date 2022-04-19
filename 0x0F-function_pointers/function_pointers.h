5569e1966fc3034f340a1a377cae5cb7669ccd1a
Hegeldirkk <hegeldirkk@gmail.com>
2022-04-19 19:00:51 +0000

    hetrhdt

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FILE_FUNC_POINT
+#define FILE_FUNC_POINT
+#include <stdlib.h>
+
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
