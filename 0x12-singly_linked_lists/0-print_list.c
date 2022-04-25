432814080393e813542c5ae5074fee1e6671919d
Hegeldirkk <hegeldirkk@gmail.com>
2022-04-25 11:49:27 +0000

    task 0 - function that prints all the elements of a list_t list.

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements list.
+ * @h: a pointer.
+ *
+ * Return: the number of nodes.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t s = 0;
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		s++;
+	}
+	return s;
+
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Singly Linked Lists
+
+## task 0
+
+## task 1
+
+## task 2
+
+## task 3
