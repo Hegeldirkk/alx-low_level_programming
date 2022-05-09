95f7db5060a0dad0d6bc8ed4ee37b8bf1927ffbf
Hegeldirkk <hegeldirkk@gmail.com>
2022-05-09 01:02:44 +0000

    task 3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,51 @@
+#include <main.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: number of type int
+ * @argv: array of type chars
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int fo_r, fo_w, x, m, n;
+	char buf[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fo_r = open(argv[1], O_RDONLY);
+	if (fo_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fo_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(fo_r, buf, BUFSIZ)) > 0)
+	{
+		if (fo_w < 0 || write(fo_w, buf, x) != x)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(fo_r);
+			exit(99);
+		}
+	}
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	m = close(fo_r);
+	n = close(fo_w);
+	if (m < 0 || n < 0)
+	{
+		if (m < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+		if (n < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
+		exit(100);
+	}
+	return (0);
+}
