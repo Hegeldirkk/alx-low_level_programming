f91274d47f902da8b9ba4c3d785e5866b2c1c33a
Hegeldirkk <hegeldirkk@gmail.com>
2022-05-14 19:58:23 +0000

    update task 101 quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints exactly word.
+ *
+ * Return: 1;
+ */
+
+int main(void)
+{
+	char *c = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
+
+	while (c[i] != '\0')
+	{
+		putchar(c[i]);
+		i++;
+	}
+	putchar('\n');
+	return (1);
+}
