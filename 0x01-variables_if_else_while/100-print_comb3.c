1a8cbb38f7f51da2a2eb4e5d0bf4d4b9246033d8
Hegeldirkk <hegeldirkk@gmail.com>
2022-05-15 19:36:42 +0000

    print combination task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints all possible of two digits.
+ * Return: 0.
+ */
+
+int main(void)
+{
+	int c = 0;
+	int b = 0;
+	int a = 0;
+
+	while (c < 10)
+	{
+		if (c != 9)
+		{
+			for (b = a; b < 10; b++)
+			{
+				putchar(48 + c);
+				if (c == b)
+					putchar(48 + ++b);
+				else
+					putchar(48 + b);
+				putchar(',');
+				putchar(' ');
+			}
+			a++;
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
