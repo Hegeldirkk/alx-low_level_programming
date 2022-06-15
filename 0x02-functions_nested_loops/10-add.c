b28ab17cedcd498d711bfe3fc68c0446d7028869
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-15 23:55:20 +0000

    update lcombg

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: first var a
+ * @b: second var b
+ *
+ * Description: adds two
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
