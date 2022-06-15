95a7f77693eea68f10ba95d400b3c0edf2d26ca1
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-15 22:53:16 +0000

    update log

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
 		for (j = 48; j < 58; j++)
 		{
-			if (i == j)
+			if (i == j || j <= i)
 			{
 				continue;
 			}
