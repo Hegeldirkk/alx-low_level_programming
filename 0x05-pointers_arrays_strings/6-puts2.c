c9593873a277f199f07171afd4735eb0de217323
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-16 00:49:49 +0000

    update 112

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,22 +3,25 @@
 /**
  * puts2 - prints one char out of 2
  * @str: type
- *
+ * Return 0
  */
 
 void puts2(char *str)
 {
-	int i, j = 0;
+	int j, i = 0;
+	char a;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-
-	while (j < i)
+	for (j = 0 ; j < i ; j++)
 	{
-		_putchar(str[j]);
-		j = j + 2;
+		if (j % 2 == 0)
+		{
+			a = *(str + j);
+			_putchar(a);
+		}
 	}
 	_putchar('\n');
 }
