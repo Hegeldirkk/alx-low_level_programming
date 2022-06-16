356c645418ed03c9e4a2f7537b1aa438f8947754
Hegeldirkk <hegeldirkk@gmail.com>
2022-06-16 00:28:00 +0000

    update 112

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,25 +10,17 @@
 
 int main(void)
 {
-	int i, n, first_count, sec_count, next_num;
+	unsigned long int i;
+	unsigned long int bef = 1;
+	unsigned long int aft = 2;
 
-	first_count = 1;
-	sec_count = 2;
-	n = 50;
+	printf("%lu", bef);
 
-	for (i = 0; i < n; i++)
+	for (i = 1; i < 50; i++)
 	{
-		if (i == 1)
-		{
-			printf("%d", first_count);
-		}
-		else
-		{
-			printf("%d, ", first_count);
-			next_num = first_count + sec_count;
-			first_count = sec_count;
-			sec_count = next_num;
-		}
+		printf(", %lu", aft);
+		aft += bef;
+		bef = aft - bef;
 	}
 	printf("\n");
 	return (0);
